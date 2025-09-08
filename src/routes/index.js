576fb50540362b60017c55d87c2996700a87cc6d
jsbmomo <tytyes40@gmail.com>
2025-09-08 16:46:54 +0900

    page is not found 적용

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,10 @@ Vue.use(VueRouter);
 export default new VueRouter({
   routes: [ // Vue router에 의해서 컨트롤(제어)되는 페이지 정보를 담는 배열
     {
+      path: '/',
+      redirect: '/login'
+    },
+    {
       path: '/login',
       // 코드 스틀리팅 적용 전, 후
       // component: LoginPage,
@@ -14,6 +18,10 @@ export default new VueRouter({
     {
       path: '/signup',
       component: () => import('@/views/SignupPage.vue')
+    },
+    {
+      path: '*', // 위의 URL를 제외한 모든 페이지에서 반응
+      component: () => import('@/views/NotFoundPage.vue')
     }
   ], 
 });
